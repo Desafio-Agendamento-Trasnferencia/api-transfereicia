33a333d3a7d41eae6ce8d0417fca5b98035252b3
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2025-08-30 15:52:14 -0300

    created entity layer

---
+++ src/main/java/desafio/apitranferencia/infrastructure/entity/ContaEntity.java
@@ -0,0 +1,47 @@
+package desafio.apitranferencia.infrastructure.entity;
+
+import desafio.apitranferencia.domain.Conta;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "contas")
+@Getter
+@Setter
+public class ContaEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String numero;
+
+    @Column(nullable = false)
+    private BigDecimal saldo;
+
+    public ContaEntity() {
+    }
+
+    public ContaEntity toEntity(Conta conta) {
+        ContaEntity entity = new ContaEntity();
+        entity.setId(conta.getId());
+        entity.setNumero(conta.getNumero());
+        entity.setSaldo(conta.getSaldo());
+        return entity;
+    }
+
+    public Conta toDomain() {
+        return new Conta(
+                this.id,
+                this.numero,
+                this.saldo
+        );
+    }
+}
---
+++ src/main/java/desafio/apitranferencia/infrastructure/entity/TransferenciaEntity.java
@@ -0,0 +1,67 @@
+package desafio.apitranferencia.infrastructure.entity;
+
+import desafio.apitranferencia.domain.StatusTransferencia;
+import desafio.apitranferencia.domain.Transferencia;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "transferencias")
+public class TransferenciaEntity {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "conta_origem_id")
+    private ContaEntity contaOrigem;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "conta_destino_id")
+    private ContaEntity contaDestino;
+
+    @Column(nullable = false)
+    private BigDecimal valor;
+
+    @Column(nullable = false)
+    private LocalDate dataAgendamento;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusTransferencia status;
+
+    @Column
+    private BigDecimal taxa;
+
+    public TransferenciaEntity() {
+    }
+
+    public TransferenciaEntity toEntity(Transferencia transferencia) {
+        TransferenciaEntity entity = new TransferenciaEntity();
+        entity.setId(transferencia.getId());
+        entity.setContaOrigem(new ContaEntity().toEntity(transferencia.getContaOrigem()));
+        entity.setContaDestino(new ContaEntity().toEntity(transferencia.getContaDestino()));
+        entity.setValor(BigDecimal.valueOf(transferencia.getValor()));
+        entity.setDataAgendamento(transferencia.getDataAgendamento());
+        entity.setStatus(transferencia.getStatus());
+        return entity;
+    }
+
+    public Transferencia toDomain(){
+        return new Transferencia(
+                this.id,
+                this.contaOrigem.toDomain(),
+                this.contaDestino.toDomain(),
+                this.valor.doubleValue(),
+                this.dataAgendamento,
+                this.status
+        );
+    }
+}
