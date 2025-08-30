fe74c2902dd0acea371f0ca5e3477d711a16027f
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2025-08-30 15:50:31 -0300

    init project

---
+++ src/main/java/desafio/apitranferencia/ApiTranferenciaApplication.java
@@ -0,0 +1,13 @@
+package desafio.apitranferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTranferenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTranferenciaApplication.class, args);
+    }
+
+}
